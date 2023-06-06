cf741202243550fecb45c8dc8773653ed1e91b2d
AlessioAbbati <alex_117@live.it>
2023-06-06 18:23:49 +0200

    add censure and text

---
+++ badwords.php
@@ -0,0 +1,29 @@
+<?php
+$def_text = 'Hai inserito:' ;
+$user_text = $_GET['text'];
+
+$censure = $_GET['censure'];
+$def_censure = 'Hai censurato:' ;
+$text_censored = str_replace($censure, '***', $user_text) ;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badwords</title>
+</head>
+
+<body>
+    <h1><?= $def_text ?></h1>
+    <p><?= $user_text ?></p>
+
+    <h2><?= $def_censure ?></h2>
+    <p><?= $text_censored ?></p>
+</body>
+
+</html>
